6a111d4cc0341490efb8a46a46635de6915d57b5
LeandroClaytonPivovarsky <leandro123clayton@gmail.com>
2024-10-08 10:44:43 -0300

    Realized queue

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 1 - Recriar o código com apenas UMA Thread \
 2 - Recriar o código com 10 Threads \
 3 - Recriar o código com 10 Threads mas usando classe.
+
+
+
+Parte(1/2):
+
+Escolher outra linguagem e replicar a atividade de thread com classe.
+
+Parte(2/2):
+
+Utilizando Ruby cada equipe irá realizar uma pesquisa referente ao compartilhamento de memória:
+
+Lockas:
+
+Semáforo:
+
+Evento:
---
+++ queue.rb
@@ -0,0 +1,54 @@
+require 'thread'
+
+class Produtor
+
+  attr_accessor :thread
+
+  def initialize(fila)
+    @fila = fila
+    @thread = Thread.new { run}
+  end
+
+  def run
+    5.times do
+      i = rand(1..1000)
+      puts "Produzido #{i}"
+
+      @fila << i
+      sleep(2)
+    end
+  end
+
+  def join
+    @thread.join
+  end
+end
+
+class Consumidor
+  def initialize(fila, targetedThread)
+    @fila = fila
+    @thread = Thread.new {run}
+    @targetedThread = targetedThread
+  end
+
+  def run
+    while @targetedThread.alive?
+      elemento = @fila.pop
+      puts "Este #{elemento} foi consumido"
+    end
+
+    puts "#{@targetedThread.alive}"
+  end
+
+  def join
+    @thread.join
+  end
+end
+
+if __FILE__ == $0
+  fila = Queue.new
+  produtor = Produtor.new(fila)
+  consumidor = Consumidor.new(fila, produtor.thread)
+  produtor.join
+
+end
